90c6bd3c142045c59a99c0339ed7a8d81215c6a6
Cory Forsyth <cory.forsyth@gmail.com>
2013-09-04 10:41:56 -0400

    Tests for callback style with build and passed arguments

--- test/tests/rsvp-tools_test.js
+++ test/tests/rsvp-tools_test.js
@@ -13,7 +13,92 @@ test("rsvpTools evented style exists", function(){
   ok(rsvpTools.evented);
 });
 
-module("rsvpTools callback style");
+module("rsvpTools callback style with build");
 
-module("rsvpTools evented style");
+var callbackApi = {
+  asyncFunction: function(successCallback, failureCallback) {
+    this.successCallback = successCallback;
+    this.failureCallback = failureCallback;
+  },
 
+  asyncWithArguments: function() {
+    this.passedArguments = Array.prototype.slice.call(arguments);
+  },
+
+  triggerSuccess: function(){
+    this.successCallback();
+  },
+
+  triggerFailure: function(){
+    this.failureCallback();
+  }
+};
+
+var Callbackable = function() {};
+Callbackable.prototype = callbackApi;
+
+var testFailed = function(){
+  ok(false, "Promise reject called when not expected");
+  start();
+};
+
+asyncTest("promise is fulfilled when success callback is invoked", function(){
+  expect(1);
+
+  var promise,
+      thennable,
+      callbackable = new Callbackable();
+
+  thennable =
+    rsvpTools.callback.build( callbackable, 'asyncFunction' );
+  promise = thennable();
+  promise.then( function(){
+    ok(true);
+    start();
+  }).fail(testFailed);
+
+  setTimeout( function(){
+    callbackable.triggerSuccess();
+  }, 0);
+});
+
+asyncTest("promise is fulfilled when failure callback is invoked", function(){
+  expect(1);
+
+  var promise,
+      thennable,
+      callbackable = new Callbackable();
+
+  thennable =
+    rsvpTools.callback.build( callbackable, 'asyncFunction' );
+  promise = thennable();
+  promise.then( testFailed, function(){
+    ok(true);
+    start();
+  });
+
+  setTimeout( function(){
+    callbackable.triggerFailure();
+  }, 0);
+});
+
+test("additional arguments are passed through to original function", function(){
+  var thennable,
+      callbackable = new Callbackable();
+
+  thennable = rsvpTools.callback.build( callbackable, 'asyncWithArguments' );
+  thennable(1,2,3);
+  deepEqual(callbackable.passedArguments.slice(0,3), [1,2,3],
+            "Passed arguments should equal [1,2,3]");
+});
+
+module("rsvpTools callback style with context");
+
+test("additional arguments are passed to the original function", function(){
+  var thennable,
+      callbackable = new Callbackable();
+
+  thennable = rsvpTools.callback.contextPassed( 'asyncWithArguments' );
+  thennable( callbackable, 1,2,3 );
+  deepEqual( callbackable.passedArguments.slice(0,3), [1,2,3] );
+});
